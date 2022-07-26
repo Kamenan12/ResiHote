0865f35bd35cd385f11c7100fcd3b01f581b1538
kamenan <kamenanantoine@gmail.com>
2022-07-26 16:41:36 +0100

    Ajout de firebase avec succes mais la version firebase 9.6.11 car la nouvelle version a un soucis

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgwmbNxFHn2Ya1tK4aojLmp5DFOyqLc-I",
+  authDomain: "dbtestresi.firebaseapp.com",
+  projectId: "dbtestresi",
+  storageBucket: "dbtestresi.appspot.com",
+  messagingSenderId: "336163662418",
+  appId: "1:336163662418:web:13b2d11ef6c663c2b00d8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
