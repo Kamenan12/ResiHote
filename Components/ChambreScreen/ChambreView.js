db6da5811e9103dfa181d970e902a180515bf38f
kamenan12 <kamenanantoine@gmail.com>
2022-10-05 18:33:41 +0000

    revenir sur un seul composant pour ajouter une nouvelle chambre afin de pourvoir facilement controller et ajouter les donnes concretement

--- Components/ChambreScreen/ChambreView.js
+++ Components/ChambreScreen/ChambreView.js
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
-
+import { useForm, Controller } from 'react-hook-form';
 import tw from 'twrnc'
 import InfoNewResi from "./InfoNewResi";
 
 
 
 const ChambreView = () => {
-    const [step, setStep] = useState(1)
+    // initialsation des constante 
+    const [step, setStep] = useState(1); // initalisation des etape
+
+    // Intialisation des champ a controller par useForm 
+    const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
+        defaultValues: {
+          nom: '',
+          description: '',
+          
+        }
+      });
 
 
+
+    // code des foncions 
     const Suivant = () => {
         setStep(step + 1)
     }
@@ -22,11 +34,46 @@ const ChambreView = () => {
         <>
             {(() => {
                 switch (step) {
+                    // code de la premiere etape 
                     case 1:
                         return (
-                        <View style={[tw`bg-white items-center`]}>
-                           <InfoNewResi  suivant={Suivant} />
-                        </View> 
+                            <View>
+                            <Text style={[{ fontSize: 50, fontWeight:"600", fontFamily:"serif"}, tw``]}> Localite dans </Text>
+                                           <View style={[tw`pt-10`]}>
+                                               <View>
+                                                   <Text style={[tw``, {color: "#7C8593", fontSize: 20}]}> Nom  </Text>
+                                                   <Controller 
+                                                   control={control}
+                                                   render={({field: {onChange, onBlur, value}}) => (
+                                                       <TextInput   
+                                                           style={styles.Input}
+                                                           onBlur={onBlur}
+                                                           onChangeText={value => onChange(value)}
+                                                           value={value}
+
+                                                       />
+                                                   
+                                                   )}
+                                                   name="nom"
+                                                   rules={{ required: true, maxLength: 50}}
+                                                   />
+                                               </View>
+                                               {errors.nom?.type === "required" && <Text style={{ color: "white", fontSize: 18}}>*Le nom est obligatoire*</Text>}
+                                               {errors.nom?.type === "minLength" && <Text style={{ color: "white", fontSize: 18}}>*Minimum de caractere 2 * </Text>}
+                                               <View>
+                                                   <Text style={[tw`pt-6`, {color: "#7C8593", fontSize: 20}]}> Description  </Text>
+                                                   <TextInput   style={[tw``, styles.Input, {}]} multiline={ true}/>
+                                               </View>
+                                               <View>
+                                                   <Text style={[tw`pt-6`, {color: "#7C8593", fontSize: 20}]}> Prix  </Text>
+                                                   <TextInput   style={[tw``, styles.Input, {}]} keyboardType={"number-pad"} />
+                                               </View>
+                                           </View>
+                                           <View style={tw`flex-row justify-evenly w-60`}>
+                                               
+                                               <Text style={tw`bg-blue-300`} onPress={()=> Suivant()}> Suivant </Text>
+                                           </View>
+                       </View>
                         );
                     case 2: 
                         return (
@@ -100,10 +147,9 @@ export default ChambreView;
 
 const styles = StyleSheet.create({
     Input: {
-        borderBottomWidth: 2,
-        borderBottomColor: "#f2f2f2",
-        
-        
+        borderWidth: 1,
+        borderColor: "black",
+        height: 50,
         fontSize: 20,
         fontWeight: "500",
         
--- Components/ChambreScreen/InfoNewResi.js
+++ Components/ChambreScreen/InfoNewResi.js
@@ -1,5 +1,6 @@
 import {View, Text, StyleSheet, TextInput} from "react-native";
 import tw from "twrnc"
+import { useForm, Controller } from 'react-hook-form';
 
 
 
@@ -11,31 +12,11 @@ import tw from "twrnc"
 
 const InfoNewResi = (props) => {
 
-
-
-
+    
 
     return (
         <View>
-             <Text style={[{ fontSize: 50, fontWeight:"600", fontFamily:"serif"}, tw``]}> Localite dans </Text>
-                            <View style={[tw`pt-10`]}>
-                                <View>
-                                    <Text style={[tw``, {color: "#7C8593", fontSize: 20}]}> Nom  </Text>
-                                    <TextInput   style={[styles.Input, {height: 40}]}/>
-                                </View>
-                                <View>
-                                    <Text style={[tw`pt-6`, {color: "#7C8593", fontSize: 20}]}> Description  </Text>
-                                    <TextInput   style={[tw``, styles.Input, {}]} multiline={ true}/>
-                                </View>
-                                <View>
-                                    <Text style={[tw`pt-6`, {color: "#7C8593", fontSize: 20}]}> Prix  </Text>
-                                    <TextInput   style={[tw``, styles.Input, {}]} keyboardType={"number-pad"} />
-                                </View>
-                            </View>
-                            <View style={tw`flex-row justify-evenly w-60`}>
-                                
-                                <Text style={tw`bg-blue-300`} onPress={()=> props.suivant()}> Suivant </Text>
-                            </View>
+
         </View>
     )
 }
