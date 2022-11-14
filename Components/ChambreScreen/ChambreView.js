9d52d472b7bb04ae57708135a971db0612a33a2d
kamenan12 <kamenanantoine@gmail.com>
2022-11-14 22:46:05 +0000

    Ajout des composant de la premiere partie de enregistrement de la residences

--- Components/ChambreScreen/ChambreView.js
+++ Components/ChambreScreen/ChambreView.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
 import { useForm, Controller } from 'react-hook-form';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { Icon, Button, BottomSheet, ListItem } from "@rneui/themed";
+import { Icon, Button, BottomSheet, ListItem, Input } from "@rneui/themed";
 import tw from 'twrnc'
 import InfoNewResi from "./InfoNewResi";
 import { itemsTypeResi } from "../Data/Data";
@@ -23,13 +23,18 @@ const ChambreView = () => {
           type_residence: '',
           Nbre_chambre: '',
           Nbre_salon:'',
-          Nbre_bain:''
+          Nbre_bain:'',
+          Capacite_acceuil: '',
           
         }
       });
 
 
-      const WatchType_residence = watch('type_residence')
+      const WatchType_residence = watch('type_residence');
+      const WatchNbre_chambre = watch('Nbre_chambre');
+      const WatchNbre_salon = watch('Nbre_salon');
+      const WatchNbre_bain = watch('Nbre_bain');
+      const WatchCapacite_acceuil = watch('Capacite_acceuil')
 
 
     //   donner pour type de residances
@@ -86,47 +91,165 @@ const ChambreView = () => {
                                     <View> 
                                         <Text> Info sur la Residences</Text>
                                     </View>
+                                    {/* View sur les type de residencs  */}
                                     <View>
-                                    <Button 
-                                        title={WatchType_residence ? WatchType_residence : "Type de Residences"}
-                                        onPress={() => setTypeResi(!typeResi)}
-                                        buttonStyle={{
-                                            backgroundColor: 'rgb(221, 221, 221)',
-                                            height:  50,
+                                        <Button 
+                                            title={WatchType_residence ? WatchType_residence : "Type de Residences"}
+                                            onPress={() => setTypeResi(!typeResi)}
+                                            buttonStyle={{
+                                                backgroundColor: 'rgb(221, 221, 221)',
+                                                height:  50,
 
-                                        }}
-                                        icon={{
-                                            name: "arrow-alt-circle-right",
-                                            type: "font-awesome-5",
-                                            size: 15,
-                                            
-                                        }}
-                                        iconContainerStyle={{
-                                            right: 70
-                                        }}
-                                        iconRight
-                                    />
-                                    <BottomSheet modalProps={{}} isVisible={typeResi}>
-                                                <Controller 
+                                            }}
+                                            icon={{
+                                                name: "arrow-alt-circle-right",
+                                                type: "font-awesome-5",
+                                                size: 15,
+                                                
+                                            }}
+                                            iconContainerStyle={{
+                                                right: 70
+                                            }}
+                                            iconRight
+                                        />
+                                        <BottomSheet modalProps={{}} isVisible={typeResi}>
+                                                    <Controller 
+                                                    control={control}
+                                                    render={({field: {onBlur, onChange, value}}) => (
+                                                    list2.map((l, i) => (
+                                                        <ListItem 
+                                                            key={i}
+                                                            containerStyle={l.containerStyle}
+                                                            onPress={value => [onChange(l.value), setTypeResi(false)]}
+                                                        >
+                                                            <ListItem.Content>
+                                                                <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+                                                            </ListItem.Content>
+                                                        </ListItem>
+                                                    ))
+                                                    )}
+                                                    name="type_residence"
+                                                    rules={{required: true}}
+                                                    />
+                                        </BottomSheet>
+            
+                                    </View>
+                                    <View>
+                                        <Text> Nombre de chambre</Text>
+                                            <Controller   
+                                            control={control}
+                                            render={({field: {onChange, onBlur,value}}) => (
+                                                <Input
+                                                placeholder="nombre de Chambre"
+                                                rightIcon={{
+                                                    type:"ionicon",
+                                                    name: "bed-outline",
+                                                    size: 50
+                                                }}
+                                                keyboardType="numeric"
+                                                
+                                                onChangeText={value => onChange(value)}
+                                                
+                                            />
+                                            )}
+                                            name="Nbre_chambre"
+                                            rules={{required: true}}
+
+                                            />
+                                    </View>
+
+                                    <View>
+                                    {
+                                        WatchType_residence === "studio" ? 
+                                            <View></View>
+                                            : (
+                                                <View>
+                                                <Text>Nombre de Salon</Text>
+                                                    <Controller   
+                                                        control={control}
+                                                        render={({field: {onChange, onBlur,value}}) => (
+                                                            <Input
+                                                            placeholder="nombre de salons"
+                                                            rightIcon={{
+                                                                type:"font-awesome-5",
+                                                                name: "chair",
+                                                                size: 40
+                                                            }}
+                                                            keyboardType="numeric"
+                                                            
+                                                            onChangeText={value => onChange(value)}
+                                                            
+                                                        />
+                                                        )}
+                                                        name="Nbre_salon"
+                                                        rules={{required: true}}
+
+                                                        />
+                                            </View>
+                                            )
+                                       }
+                                    </View>
+
+                                    <View>
+                                        <Text> Nombre de salle de bain</Text>
+
+                                        <Controller   
+                                            control={control}
+                                            render={({field: {onChange, onBlur,value}}) => (
+                                                <Input
+                                                placeholder="nombre de salle de bain"
+                                                rightIcon={{
+                                                    type:"font-awesome",
+                                                    name: "bathtub",
+                                                    size: 40
+                                                }}
+                                                keyboardType="numeric"
+                                                
+                                                onChangeText={value => onChange(value)}
+                                                
+                                            />
+                                            )}
+                                            name="Nbre_bain"
+                                            rules={{required: true}}
+                                            />
+                                    </View>
+
+                                    <View>
+                                        <Text> Capacite d'accueil</Text>
+                                                    
+                                            <Controller   
                                                 control={control}
-                                                render={({field: {onBlur, onChange, value}}) => (
-                                                list2.map((l, i) => (
-                                                    <ListItem 
-                                                        key={i}
-                                                        containerStyle={l.containerStyle}
-                                                        onPress={value => [onChange(l.value), setTypeResi(false)]}
-                                                    >
-                                                        <ListItem.Content>
-                                                            <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-                                                        </ListItem.Content>
-                                                    </ListItem>
-                                                ))
-                                                )}
-                                                name="type_residence"
-                                                rules={{required: true}}
+                                                render={({field: {onChange, onBlur,value}}) => (
+                                                    <Input
+                                                    placeholder="nombre de personnes"
+                                                    rightIcon={{
+                                                        type:"foundation",
+                                                        name: "torsos-all",
+                                                        size: 40
+                                                    }}
+                                                    keyboardType="numeric"
+                                                    
+                                                    onChangeText={value => onChange(value)}
+                                                    
                                                 />
-                                            </BottomSheet>
+                                                )}
+                                                name="Capacite_acceuil"
+                                                rules={{required: true}}
+                                            />
+                                    </View>
+                                    <View>
+                                    {
+                                        WatchType_residence === '' | WatchNbre_chambre === '' | WatchCapacite_acceuil === '' | WatchNbre_salon === '' ?
 
+                                        <Button title="suivant" 
+                                        disabled={true}
+                                        /> :
+
+                                        <Button 
+                                        title="suivant" 
+                                        onPress={() => Suivant()}
+                                        />
+                                    }
                                     </View>
                                 </View>
                             </View>
