3d6d8dcaea8fe584ce88a6937866bfc9b534feec
kamenan12 <kamenanantoine@gmail.com>
2022-11-16 15:10:38 +0000

    Ajout de la deuxeime page avec succes

--- Components/ChambreScreen/ChambreView.js
+++ Components/ChambreScreen/ChambreView.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native";
 import { useForm, Controller } from 'react-hook-form';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { Icon, Button, BottomSheet, ListItem, Input } from "@rneui/themed";
+import { Icon, Button, BottomSheet, ListItem, Input, CheckBox } from "@rneui/themed";
 import tw from 'twrnc'
 import InfoNewResi from "./InfoNewResi";
 import { itemsTypeResi } from "../Data/Data";
@@ -16,7 +16,17 @@ const ChambreView = () => {
 
     // constante pour les ouverture de modal
     const [typeResi, setTypeResi] = useState(false)
+    const [equipBase, setEquipBase] = useState(false)
+    const [equipExtra, setEquipExtra] = useState(false)
 
+    // state de equipement de basee
+    const [tv, setTv] = useState(false);
+    const [refrigerateur, setRefrigerateur] = useState(false);
+    const [climatiseur, setClimatiseur] = useState(false)
+    // Equipement extra 
+    const [jardin, setJardin] = useState(false)
+    const [garage, setGarage] = useState(false)
+ 
     // Intialisation des champ a controller par useForm 
     const { register, watch, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
         defaultValues: {
@@ -25,6 +35,9 @@ const ChambreView = () => {
           Nbre_salon:'',
           Nbre_bain:'',
           Capacite_acceuil: '',
+          Equipement_bases: [],
+          Equipement_extra: [],
+          Prix: '',
           
         }
       });
@@ -34,8 +47,9 @@ const ChambreView = () => {
       const WatchNbre_chambre = watch('Nbre_chambre');
       const WatchNbre_salon = watch('Nbre_salon');
       const WatchNbre_bain = watch('Nbre_bain');
-      const WatchCapacite_acceuil = watch('Capacite_acceuil')
-
+      const WatchCapacite_acceuil = watch('Capacite_acceuil');
+      const WatchEquipement_bases = watch('Equipement_bases');
+      const WatchEquipement_extra = watch('Equipement_extra');
 
     //   donner pour type de residances
      
@@ -78,6 +92,62 @@ const ChambreView = () => {
             onPress: () => setTypeResi(false),
         },
        ]
+    const ItemsEquipBase = [
+        {
+            title: 'Tv',
+            value: 'Televison',
+            checked: tv,
+            onPressed:  setTv,
+            // containerStyle: {
+            //     borderWidth: 2,
+            //     borderColor: "red",
+            //     paddingVertical: 0
+            // }
+    
+        },
+        {
+            title: 'Refrigerateur',
+            value: 'Refrigerateur',
+            checked: refrigerateur,
+            onPressed: setRefrigerateur,
+    
+        },
+        {
+            title: 'Climatiseur',
+            value: 'Climatiseur',
+            checked: climatiseur,
+            onPressed: setClimatiseur,
+        },
+        {
+            title: 'Validez',
+            containerStyle: { backgroundColor: 'orange' },
+            titleStyle: { color: 'white' },
+            onPressed: () => setEquipBase(false),
+        },
+       ]
+
+    const ItemsEquiExtra = [
+        {
+            title: 'Jardin',
+            value: 'Jaridin',
+            checked: jardin,
+            onPressed: setJardin
+        },
+        {
+            title: 'Garage',
+            value: 'garage',
+            checked: garage,
+            onPressed: setGarage,
+        },
+        {
+            title: 'Validez',
+            containerStyle: { backgroundColor: 'orange' },
+            titleStyle: { color: 'white' },
+            onPressed: () => setEquipExtra(false),
+        },
+       ]
+
+       //// fin des donneees 
 
     return (
         <>
@@ -256,15 +326,188 @@ const ChambreView = () => {
                         );
                     case 2: 
                         return (
-                            <View style={[tw`bg-white items-center`]}>
-                            <Text >  les champ de chambre etape {step}</Text>
+                        <View >
                             <View>
-                               
+                                <Text>Etape 2</Text>
+                            </View>
+                            <View>
+                                <Text> Equipement de base </Text>
+                                    <Button 
+                                    title="equipement de base"
+                                    onPress={() => setEquipBase(!equipBase)}
+                                    buttonStyle={{
+                                        backgroundColor: 'rgb(221, 221, 221)',
+                                        height:  50,
+
+                                    }}
+                                    icon={{
+                                        name: "arrow-alt-circle-right",
+                                        type: "font-awesome-5",
+                                        size: 15,
+                                        
+                                    }}
+                                    iconContainerStyle={{
+                                        right: 70
+                                    }}
+                                    iconRight
+                                    />
+
+                                        <BottomSheet modalProps={{}} isVisible={equipBase}>
+                                            <Controller 
+                                            control={control}
+                                            render={({field: {onChange, onBlur, value}}) => (
+                                                
+                                                    ItemsEquipBase.map((l, i) => (
+                                                        <ListItem
+                                                        key={i}
+                                                        containerStyle={l.containerStyle}
+                                                        
+                                                        >
+                                                            
+                                                                {
+                                                                    l.title === 'Validez' ? 
+                                                                    <ListItem.Content
+                                                                     >
+                                                                    <ListItem.Title style={l.titleStyle} onPress={l.onPressed}>{l.title}</ListItem.Title>
+                                                                </ListItem.Content> :
+                                                                <ListItem.Content>
+                                                                    <CheckBox 
+                                                                    center
+                                                                    title={l.title}
+                                                                    onPress={() => [l.onPressed(!l.checked), 
+                                                                        l.checked ? 
+                                                                        
+                                                                        onChange(WatchEquipement_bases.filter(chek => chek !== l.value))
+                                                                        
+                                                                        : 
+                                                                         onChange([...WatchEquipement_bases, l.value]) ]}
+                                                                    checked={l.checked}
+                                                                    />
+                                                                </ListItem.Content>
+                                                                }
+                                                                
+                                                            
+                                                        </ListItem>
+                                                    ))
+                                                
+                                             )} 
+                                            
+                                            name="Equipement_bases"
+                                            
+                                            /> 
+                                        </BottomSheet>
+
+                                        <View>
+                                        <View>
+                                        {WatchEquipement_bases.map((e, i) => (
+                                                <Text key={i}>-{e}</Text>
+                                            ))}
+                                        </View>
+                                        </View>
+                            </View>
+                            <View>
+                            <Text> Equipement extra </Text>
+                                <Button 
+                                title="equipement Extra"
+                                onPress={() => setEquipExtra(!equipExtra)}
+                                buttonStyle={{
+                                    backgroundColor: 'rgb(221, 221, 221)',
+                                    height:  50,
+
+                                }}
+                                icon={{
+                                    name: "arrow-alt-circle-right",
+                                    type: "font-awesome-5",
+                                    size: 15,
+                                    
+                                }}
+                                iconContainerStyle={{
+                                    right: 70
+                                }}
+                                iconRight
+                                />
+
+                                <BottomSheet modalProps={{}} isVisible={equipExtra}>
+                                    <Controller 
+                                    control={control}
+                                    render={({field: {onChange, onBlur, value}}) => (
+                                        
+                                            ItemsEquiExtra.map((l, i) => (
+                                                <ListItem
+                                                key={i}
+                                                containerStyle={l.containerStyle}
+                                                
+                                                >
+                                                    
+                                                        {
+                                                            l.title === 'Validez' ? 
+                                                            <ListItem.Content
+                                                                >
+                                                            <ListItem.Title style={l.titleStyle} onPress={l.onPressed}>{l.title}</ListItem.Title>
+                                                        </ListItem.Content> :
+                                                        <ListItem.Content>
+                                                            <CheckBox 
+                                                            center
+                                                            title={l.title}
+                                                            onPress={() => [l.onPressed(!l.checked), 
+                                                                l.checked ? 
+                                                                
+                                                                onChange(WatchEquipement_extra.filter(chek => chek !== l.value))
+                                                                
+                                                                : 
+                                                                    onChange([...WatchEquipement_extra, l.value]) ]}
+                                                            checked={l.checked}
+                                                            />
+                                                        </ListItem.Content>
+                                                        }
+                                                        
+                                                    
+                                                </ListItem>
+                                            ))
+                                        
+                                        )} 
+                                    
+                                    name='Equipement_extra'
+                                    
+                                    /> 
+                                </BottomSheet>
+                                <View>
+                                    {WatchEquipement_extra.map((e, i) => (
+                                            <Text key={i}>-{e}</Text>
+                                        ))}
+                                </View>
+                            </View>
+                            <View >
+                                <Text> Prix/24h</Text>
+                                    <Controller   
+                                    control={control}
+                                    render={({field: {onChange, onBlur,value}}) => (
+                                        <Input
+                                        placeholder="Prix"
+                                        rightIcon={{
+                                            type:"ionicon",
+                                            name: "md-pricetag",
+                                            size: 30
+                                        }}
+                                        keyboardType="numeric"
+                                        
+                                        onChangeText={value => onChange(value)}
+                                        
+                                    />
+                                )}
+                                name="Prix"
+                                rules={{required: true}}
+
+                                />
                             </View>
                             <View style={tw`flex-row justify-evenly w-60`}>
                                 
-                                <Text style={tw`bg-blue-300`} onPress={()=> Precedent()}> precedent </Text>
-                                <Text style={tw`bg-blue-300`} onPress={()=> Suivant()}> Suivant </Text>
+                                <Button title="suivant"
+                                    onPress={() => Suivant()} 
+                                />
+                                <Button title="precedent"
+                                    onPress={() => Precedent()} 
+                                />
                             </View>
                         </View> 
                         )
