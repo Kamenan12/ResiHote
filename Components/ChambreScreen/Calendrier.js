c192e8a9fa7d007fd4cff9ed8cd16ad6a95d362f
kamenan12 <kamenanantoine@gmail.com>
2022-11-28 09:13:47 +0000

    ajout de fonction sup pour supprimer une date deja ajoute dans le calendrier

--- Components/ChambreScreen/Calendrier.js
+++ Components/ChambreScreen/Calendrier.js
@@ -6,7 +6,7 @@ import CalendarPicker from 'react-native-calendar-picker';
 import moment from "moment";
 import { set } from "react-hook-form";
 
-
+import tw from 'twrnc'
 
 
 
@@ -24,7 +24,49 @@ const Calendrier = () => {
     const NbreDate = moment(endDate).diff(startDate, 'day');
     // const rang = dispo ? moment(startDate).add
     // const dur = endDate ? moment().startOf(startDate, 'day').from(endDate, 'day'): "";
+    // const [disable, setDisable] = useState([])
+    // ran.map( (dat) => (
+    //     dat.map((d) => (
+    //         disable.push(d.jour,)
+    //     ))
+    // ))
 
+    ////*****//////////////// */
+    const DisableDay = []
+    ran.map( (dat) => (
+        dat.map((d) => (
+            DisableDay.push(d.jour,)
+        ))
+    ))
+    // const test = [
+    // "2022-11-25",
+    // "2022-11-26",
+    // "2022-11-27",
+    // "2022-11-28",
+    // "2022-11-29",
+    // "2022-11-30",]
+
+    const test2 = ['James', 'John', 'Paul', 'Ringo', 'George'];
+    // pour le Css des date selecionne
+    const CustumStyleDate = [] 
+    ran.map( (dat) => (
+        dat.map((d) => (
+            CustumStyleDate.push({
+                date: d.jour,
+                style: {
+                    backgroundColor: 'red'
+                },
+                textStyle: {
+                    color: 'white'
+                }, 
+                containerStyle: {
+                    backgroundColor: 'green'
+                },
+                // allowDisabled: false
+
+            })
+        ))
+    ))
 
     const onDateChange = (date, type) => {
         if (type === 'END_DATE') {
@@ -80,6 +122,47 @@ const Calendrier = () => {
         console.log("raaann", ran)
     }
 
+    const Sup = (d) => {
+        // DisableDay.filter(j => j !== jour)
+        // ran.filter(j => j.jour != d)
+        // console.log('dd', d) 
+
+        // setRan(ran.filter(tab1 => tab1.find(jo => jo.jour !==  d)))
+        // DisableDay.filter(tab => tab != d)
+        // console.log("filt", DisableDay)
+        // console.lo
+        // test.filter(tab => tab !== d.jour)
+        // let franc = test2.filter(tab => tab == "James")
+        // console.log("Dise", DisableDay)
+        // let franc = DisableDay.filter(tab => tab !== d)
+        // console.log("Desdd", DisableDay)
+        // console.log("test", test)
+        // console.log("test1222", test2)
+        // console.log("franc", franc)
+        // console.log("ddd", d)
+        // if (d) {
+        //     ran.map((m, key) => { 
+        //         m.filter(dis => dis !== d)
+        //     })
+        // }
+        let c2 = []
+        let cc
+        console.log("ranSupp", ran)
+        ran.map((dis, key) => {
+              cc = dis.filter(di => di.jour !== d)
+              c2 = [...c2, cc]
+        })
+        console.log("cc", cc)
+        console.log("222", c2)
+        // let ccc = ran.filter(
+        //     dis => {
+        //        console.log("filtre", dis[0])
+        //     }
+        // )
+        // console.log("ccc", ccc)
+        // console.log("ccc", ccc)
+        setRan(c2)
+    } 
 
     return (
         <View>
@@ -94,18 +177,8 @@ const Calendrier = () => {
                 previousTitle="Precedent"
                 nextTitle="Suivant"
                 weekdays={['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'sam', 'Dim']}
-                customDatesStyles={[{
-                    date: '2022-11-26',
-                    style: {
-                        backgroundColor: 'red'
-                    },
-                    textStyle: {
-                        color: 'white'
-                    }, 
-                    containerStyle: {
-                        backgroundColor: 'green'
-                    }
-                }]}
+                customDatesStyles={CustumStyleDate}
+                disabledDates={DisableDay}
             />
 
             <View>
@@ -118,22 +191,41 @@ const Calendrier = () => {
                 { dispo.map((d, key) => (
                     <View key={key}>
                         <Text > du: {d.debut}</Text>
-                        <Text > au: {d.fin}</Text>
+                        <Text > au: {d.fin}</Text> 
                     </View>
                 ))}
                 {/* <Text>{ran.length}</Text> */}
+                {/* { DisableDay.map((d, key) => (
+                    <View key={key}>
+                        <View key={key} style={tw`flex-row pt-2`}>
+                                <Text> jour: {d}</Text>  
+                                <Text style={tw`bg-red-300 ml-2`} onPress={() => Sup(d)}>Supprimer </Text>
+                            </View>
+                    </View>
+                ))} */}
                 { ran.map((d, key) => (
                     <View key={key}>
                         <View>
-                            <Text>jour {d.jour}</Text>
+                            {/* <Text>jour {d.jour}</Text> */}
                             {d.map((r,key) => (
-                            <View key={key}>
-                                <Text> jou:2 {r.jour}</Text> 
+                            <View key={key} style={tw`flex-row pt-2`}>
+                                <Text> jour: {r.jour}</Text>  
+                                <Text style={tw`bg-red-300 ml-2`} onPress={() => Sup(r.jour)}>Supprimer </Text>
                             </View>
                             ))}
                         </View>
                     </View>
                 ))}
+
+                {/* <View>
+                    <Text> ICICCI test filte</Text>
+                    {DisableDay.map((t, key) => (
+                        <View key={key} style={tw`flex-row`}> 
+                            <Text> -{t} </Text>
+                            <Text style={tw`bg-red-300 ml-2`} onPress={() => Sup(t)}>Supprimer </Text>
+                        </View>
+                    ))}
+                </View> */}
             </View>
         </View>
     )
