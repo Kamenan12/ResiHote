7ec70c6258b78c203d3b55ab008cc7a2cc3ee99c
kamenan12 <kamenanantoine@gmail.com>
2023-01-17 23:35:53 +0000

    initiation de modification du calendrier

---
+++ Components/ResidenceScreens/Details/Modif/Calendrier.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from "react-native";
+
+
+
+
+
+
+const CalendrierModif = () => {
+    return (
+
+        <View>
+            <Text>ici modifie calendrier </Text>
+        </View>
+    )
+}
+
+
+
+export default CalendrierModif;
+
+
+
+
+const styles = StyleSheet.create({
+    
+})
