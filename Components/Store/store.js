a59e1ed101effc2da5db2b31c80064eb52636efa
kamenan12 <kamenanantoine@gmail.com>
2023-08-08 17:04:36 +0000

    Essayons de changer le nombre de vue en nombre de reservation

--- Components/Store/store.js
+++ Components/Store/store.js
@@ -4,5 +4,9 @@ import getHoteSlice from './getHoteSlice'
 export const store = configureStore({
   reducer: {
     hote: getHoteSlice,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    //   immutableCheck: false,
+    //   serializableCheck: false,
+    // })
   },
 })
