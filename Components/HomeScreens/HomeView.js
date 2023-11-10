ab4d06ef516c63ed9008261059f04797d9880faf
kamenan12 <kamenanantoine@gmail.com>
2023-11-10 10:16:01 +0000

    suupression de netInfo succes, essayons de build app

--- Components/HomeScreens/HomeView.js
+++ Components/HomeScreens/HomeView.js
@@ -1,7 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 import { signOut, getAuth } from "firebase/auth";
-import {useNetInfo} from '@react-native-community/netinfo';
 import GlobalStyle from "../Styles/GlobalStyle";
 import tw from "twrnc"
 import BarEntete from "./BarEntete";
