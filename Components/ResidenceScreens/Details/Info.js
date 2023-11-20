2edf5a3512311d136fd3d551559f36a5c60ec02d
kamenan12 <kamenanantoine@gmail.com>
2023-11-20 21:04:48 +0000

    Repris Ajout de valeur de saisir par default dans modification des information de Residences

--- Components/ResidenceScreens/Details/Info.js
+++ Components/ResidenceScreens/Details/Info.js
@@ -63,46 +63,48 @@ const Info = (props) => {
         modif === false ? (
 
         <>
-             <View style={tw`flex-row justify-between`}>
-                        <Text style={[{ fontSize: 33, fontWeight: "700", fontFamily: "serif"}]}>{props.Titre} </Text>
-             </View>
-             <View style={tw`flex-row`}>
-                        
-                        <View style={tw`flex-row`}>
-                            <Icon  name="location" color="black" type="entypo" size={25}/>
-                            <Text style= {[{fontSize: 17, fontWeight: "700", fontFamily:"sans-serif", color: "black"}]}> {props.Localite.ville}</Text>
-                        </View>
-                        <View style={tw`flex-row`}>
-                                  
-                            <Text style= {[{fontSize: 17, fontWeight: "700", fontFamily:"sans-serif", color: "black"}]}> {props.Localite.commune}</Text>
-                            {/* <Text style={[{color:"gray"}]}> un bon quatier près</Text> */}
-                        </View>
-                    </View>
-                        <View style={tw`flex-row`}>
-                        <Icon  name="location-pin" color="black" type="entypo" size={25}/> 
-                            <Text style={[{fontSize: 15, fontWeight: '500'}]}> {props.Localite.description}</Text>
-                        </View>
-             
-                    <View style={tw`pt-5`}>
-                        <Text style={[{color: "gray", fontWeight: "700", fontSize: 15}]}> {props.Description} </Text>
-                    </View>
-                    <View style={tw`items-end pt-5 px-5`}>
-                        <Button title="modifier" onPress={() => SetModif(true)}
-                            buttonStyle={tw`bg-transparent border rounded-xl`}
-                            titleStyle={{
-                                color: "red"
-                            }}
-                            // icon={{
-                            //     name: "cancel",
-                            //     type: "material",
-                            //     size: 15,
-                            //     color: "red"
-                                
-                            // }}
+            <View style={tw`px-5`}>
+                <View style={tw`flex-row justify-between`}>
+                            <Text style={[{ fontSize: 33, fontWeight: "700", fontFamily: "serif"}]}>{props.Titre} </Text>
+                </View>
+                <View style={tw`flex-row`}>
                             
-                            // iconRight
-                        />
-                    </View>
+                            <View style={tw`flex-row`}>
+                                <Icon  name="location" color="black" type="entypo" size={25}/>
+                                <Text style= {[{fontSize: 17, fontWeight: "700", fontFamily:"sans-serif", color: "black"}]}> {props.Localite.ville}</Text>
+                            </View>
+                            <View style={tw`flex-row`}>
+                                    
+                                <Text style= {[{fontSize: 17, fontWeight: "700", fontFamily:"sans-serif", color: "black"}]}> {props.Localite.commune}</Text>
+                                {/* <Text style={[{color:"gray"}]}> un bon quatier près</Text> */}
+                            </View>
+                        </View>
+                            <View style={tw`flex-row`}>
+                            <Icon  name="location-pin" color="black" type="entypo" size={25}/> 
+                                <Text style={[{fontSize: 15, fontWeight: '500'}]}> {props.Localite.description}</Text>
+                            </View>
+                
+                        <View style={tw`pt-5`}>
+                            <Text style={[{color: "gray", fontWeight: "700", fontSize: 15}]}> {props.Description} </Text>
+                        </View>
+                        <View style={tw`items-end pt-5 px-5`}>
+                            <Button title="modifier" onPress={() => SetModif(true)}
+                                buttonStyle={tw`bg-transparent border rounded-xl`}
+                                titleStyle={{
+                                    color: "red"
+                                }}
+                                // icon={{
+                                //     name: "cancel",
+                                //     type: "material",
+                                //     size: 15,
+                                //     color: "red"
+                                    
+                                // }}
+                                
+                                // iconRight
+                            />
+                        </View>
+            </View>
         </>
         ) :  (
 
@@ -113,20 +115,21 @@ const Info = (props) => {
                             control={control}
                             render={({field: {onChange, onBlur,value}}) => (
                                 <Input
-                                    placeholder="Titre de l'annonce "
+                                    placeholder="Titre de l'annon "
                                     
                                     onChangeText={value => onChange(value)}
-                                    value={value}
+                                    
+                                    defaultValue={props.Titre}
                                 />
                             )}
                             name="Titre"
-                            rules={{ required: true, minLength: 10}}
+                            rules={{ required: true, minLength: 2}}
                             />
 
                             {/* {errors.Titre?.type === "required" && <Text style={{  fontSize: 18}}>*Le Titre est obligatoire*</Text>} */}
                             {/* {errors.nom?.type === "pattern" && <Text style={{ color: "white", fontSize: 18}}>* Pas de caractere special * </Text>} */}
-                            {errors.Titre?.type === "minLength" && <Text style={{ fontSize: 15}}>*Minimum de caractere 10 * </Text>}
-                            {errors.Titre?.type === "required" && <Text style={{  fontSize: 18}}>*Le Titre est obligatoire*</Text>}
+                            {errors.Titre?.type === "minLength" && <Text style={{ fontSize: 15, color: "red"}}>*Minimum de caractere 2 * </Text>}
+                            {errors.Titre?.type === "required" && <Text style={{  fontSize: 18, color: "red"}}>*Le Titre est obligatoire*</Text>}
                             
                     </View>
                     <View>
@@ -140,6 +143,7 @@ const Info = (props) => {
                                 numberOfLines={6}
                                 maxLength={250}
                                 onChangeText={value => onChange(value)}
+                                defaultValue={props.Localite.description}
                                 // containerStyle={{
                                 //     height: 120,
                                 // }}
@@ -152,12 +156,12 @@ const Info = (props) => {
                             // <TextArea />
                             )}
                             name="Description"
-                            rules={{required: true, maxLength: 250, minLength: 30}}
+                            rules={{required: true, maxLength: 250, minLength: 10}}
                             />
 
-                            {errors.Description?.type === "required" && <Text style={{  fontSize: 18}}>*La Description est obligatoire*</Text>}
-                            {errors.Description?.type === "minLength" && <Text style={{ fontSize: 15}}>*Minimum de caractere 30 * </Text>}
-                            {errors.Description?.type === "maxLength" && <Text style={{  fontSize: 18}}>*Le Maximun de caractere est 250*</Text>}
+                            {errors.Description?.type === "required" && <Text style={{  fontSize: 18, color: "red"}}>*La Description est obligatoire*</Text>}
+                            {errors.Description?.type === "minLength" && <Text style={{ fontSize: 15, color: "red"}}>*Minimum de caractere 10 * </Text>}
+                            {errors.Description?.type === "maxLength" && <Text style={{  fontSize: 18, color: "red"}}>*Le Maximun de caractere est 250*</Text>}
                     </View>
                     <View style={tw`flex-row w-80 justify-between pt-5 px-5`}>
                         <Button title="annueler" onPress={() => SetModif(false)}
